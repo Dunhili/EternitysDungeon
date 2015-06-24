b1fad93a20802f7cd36dd96dce1bcb5f52ddc04a
Brian Bowden <bbowden1@vt.edu>
2015-06-24 14:55:33 -0400

    Update AbilityType.java

--- app/src/main/java/com/dunhili/eternitysdungeon/ability/AbilityType.java
+++ app/src/main/java/com/dunhili/eternitysdungeon/ability/AbilityType.java
@@ -4,5 +4,21 @@ package com.dunhili.eternitysdungeon.ability;
  * Created by Dunhili on 5/27/2015.
  */
 public enum AbilityType {
-    SINGLE, CONE, CIRCLE, LINE, PASSIVE, AURA;
+    /** Ability is a single target. */
+    SINGLE, 
+    
+    /** Ability hits units in a cone shape. */
+    CONE, 
+    
+    /** Ability hits units in a circle originating from a certain point. */
+    CIRCLE,
+    
+    /** Ability hits units in a line. */
+    LINE, 
+    
+    /** Ability that does not need to be cast and is usually always on. */
+    PASSIVE, 
+    
+    /** Passive ability that also extends outward from the user. */
+    AURA;
 }
